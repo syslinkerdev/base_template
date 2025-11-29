19fe2aeeff80888199f059ab7c7fd33255c1fb0b
syslinkerdev <syslinker.dev@gmail.com>
2025-11-29 17:14:46 +0530

    removed some comments from main file.

--- apps/dairyB2b_management/lib/main.dart
+++ apps/dairyB2b_management/lib/main.dart
@@ -2,8 +2,3 @@ import 'package:dairyB2b_management/app/app.dart';
 import 'package:dairyB2b_management/bootstrap.dart';
 
 Future<void> main() async => await bootstrap(() => const App());
-
-
-// Platform  Firebase App Id
-// android   1:294130841953:android:5c1dd9a6f51674cc6a6d20
-// ios       1:294130841953:ios:8506390820f717286a6d20
